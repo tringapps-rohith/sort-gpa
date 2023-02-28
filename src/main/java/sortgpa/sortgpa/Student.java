6c7044c22150006acaeb793c897c9540e4162f73
tringapps-rohith <123439228+tringapps-rohith@users.noreply.github.com>
2023-02-28 14:19:08 +0530

    Update Student.java

--- src/main/java/sortgpa/sortgpa/Student.java
+++ src/main/java/sortgpa/sortgpa/Student.java
@@ -2,49 +2,6 @@ package sortgpa.sortgpa;
 
 import java.util.*;
 import java.util.logging.*;
-class StudentGPA
-{
-        String name;
-        int age;
-        double gpa;
-        public StudentGPA(String name,int age,double gpa)
-        {
-                this.name=name;
-                this.age=age;
-                this.gpa=gpa;
-        }
-        public StudentGPA()
-        {
-        }
-        public void setName(String name)
-        {
-                this.name=name;
-        }
-        public String getName()
-        {
-                return this.name;
-        }
-        public void setAge(int age)
-        {
-                this.age=age;
-        }
-        public int getAge()
-        {
-                return this.age;
-        }
-        public void setGPA(double gpa)
-        {
-                this.gpa=gpa;
-        }
-        public double getGPA()
-
-        {
-                return this.gpa;
-        }
-        public String toString(){
-                return this.name+" "+this.age+" "+this.gpa;
-        }
-}
 class StudentTest
 {
         public static void main(String...args)
@@ -96,8 +53,8 @@ class StudentTest
                                                 {
                                                         return 0;
                                                 }
-												return -1;
-										}
+						return -1;
+					}
                                 });
                 l.info("\nStudents sorted by their GPA");
                 for (int j=0;j<n;j++)
